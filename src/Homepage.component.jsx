ce7a8c4a0f1d0439c52828b5bf3dda65c1eb7000
steven_kirwan <kirwan.ste@gmail.com>
2019-08-28 21:30:04 +0100

    created project with CRA and created hompege component

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+// styles
+import "./Homepage.styles.scss";
+
+export const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Hats</h1>
+          <p className="subtitle">SHOP NOW</p>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jacket</h1>
+          <p className="subtitle">SHOP NOW</p>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Shoes</h1>
+          <p className="subtitle">SHOP NOW</p>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Mens</h1>
+          <p className="subtitle">SHOP NOW</p>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Mens</h1>
+          <p className="subtitle">SHOP NOW</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
